e708f01f7abdfc7a5719c4e7199f7588f5cac449
Daniel Gaytan <dnigaytan07@gmail.com>
2019-11-17 22:58:06 -0600

    Added model.js

---
+++ model.js
@@ -0,0 +1,47 @@
+let mongoose = require('mongoose')
+
+var blogSchema = mongoose.Schema(
+  {
+    id : {
+      type : String,
+    },
+    title : {
+      type : String,
+    },
+    author : {
+      type : String,
+    },
+    content : {
+      type : String,
+    },
+    publishDate : {
+      type : String,
+    },
+  });
+
+let blogHandler = mopngoose.model( 'blogHandler', blogSchema);
+
+var blogFunctionalities = {
+
+  get : () => {
+    return blogHandler.find().then((posts) => {return posts}).catch((e) => {throw Error(e)});
+  },
+
+  getByAuthor : (authorName) => {
+    return blogHandler.find({author: authorName}).then((post) => {return post}).catch((e) => {throw Error(e)});
+  },
+
+  post : (blogEntry) => {
+    return blogHandler.create(blogEntry).then((post) => {return post}).catch((e) => {throw Error(e)});
+  },
+
+  put : (blogToUpdate) => {
+    return blogHandler.findOneAndUpdate({id : blogToUpdate.id}, blogToUpdate).then((post) => {return post}).catch((e) => {throw Error(e)});
+  },
+
+  delete : (blogToDelete) => {
+    return blogHandler.findOneAndRemove({id : blogToDelete.id}).then((post) => {return post}).catch((e) => { throw Error(e)});
+  }
+};
+
+module.exports = {blogFunctionalities};
