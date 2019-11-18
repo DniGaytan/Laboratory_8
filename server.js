008b23ae1b680e85cfd6e117e20cc725757e60cb
Daniel Gaytan <dnigaytan07@gmail.com>
2019-11-18 00:45:34 -0600

    more changes

--- server.js
+++ server.js
@@ -135,27 +135,46 @@ app.put('/blog-posts/:id', jsonP, function(req, res){
 
 });
 
+function runServer(port, databaseUrl){
+	return new Promise( (resolve, reject ) => {
+		mongoose.connect(databaseUrl, response => {
+			if ( response ){
+				return reject(response);
+			}
+			else{
+				server = app.listen(port, () => {
+					console.log( "App is running on port " + port );
+					resolve();
+				})
+				.on( 'error', err => {
+					mongoose.disconnect();
+					return reject(err);
+				})
+			}
+		});
+	});
+}
 
 function closeServer(){
-     return mongoose.disconnect()
-      	 .then(() => {
-        		 return new Promise((resolve, reject) => {
-          			 console.log('Closing the server');
-          			 server.close( err => {
-          				 if (err){
-          				 	     return reject(err);
-          				 }
-          				 else{
-          				 	     resolve();
-          				 }
-          			 });
-      		 });
-    	 });
+	return mongoose.disconnect()
+		.then(() => {
+			return new Promise((resolve, reject) => {
+				console.log('Closing the server');
+				server.close( err => {
+					if (err){
+						return reject(err);
+					}
+					else{
+						resolve();
+					}
+				});
+			});
+		});
 }
 
 runServer( PORT, DATABASE_URL )
-	.catch( (e) => {
-		console.log( e );
+	.catch( err => {
+		console.log( err );
 	});
 
 module.exports = { app, runServer, closeServer };
